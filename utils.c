8233ac133c648f63cbce60ae4d50e37976755789
Ahmd Fahs <afahs@c2r2s5.42beirut.com>
2025-07-20 14:03:18 +0300

    jnub

--- utils.c
+++ utils.c
@@ -1,22 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afahs <afahs@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/11 10:18:15 by afahs             #+#    #+#             */
-/*   Updated: 2025/07/20 14:01:33 by afahs            ###   ########.fr       */
+/*   Created: 2025/07/11 11:50:53 by afahs             #+#    #+#             */
+/*   Updated: 2025/07/20 13:59:15 by afahs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+int	ft_strncmp(char *s1, char *s2, size_t n)
+{
+	size_t	i;
 
-char	**ft_split(char const *s, char c);
-void	free_split(char **split);
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
+}
 
-#endif
+void	free_split(char **split)
+{
+	size_t	i;
+
+	i = 0;
+	if (!split)
+		return ;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
