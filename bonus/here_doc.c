2442c29c6642ade640db71aa42d2311153034d38
Ahmd Fahs <81890799+xiiibits@users.noreply.github.com>
2025-07-30 19:40:34 +0300

    bonus

---
+++ bonus/here_doc.c
@@ -0,0 +1,84 @@
+
+#include "pipex_bonus.h"
+
+static char	*get_next_line(void)
+{
+	char	*line;
+	char	buffer;
+	int		i;
+	int		bytes_read;
+
+	line = malloc(1000);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (1)
+	{
+		bytes_read = read(STDIN_FILENO, &buffer, 1);
+		if (bytes_read <= 0)
+			break;
+		if (buffer == '\n')
+		{
+			line[i] = '\0';
+			return (line);
+		}
+		line[i] = buffer;
+		i++;
+		if (i >= 999)
+			break;
+	}
+	line[i] = '\0';
+	if (i == 0 && bytes_read <= 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+static int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+int	create_here_doc(char *limiter)
+{
+	int		pipe_fd[2];
+	char	*line;
+	pid_t	pid;
+
+	if (pipe(pipe_fd) == -1)
+		error_exit("Failed to create here_doc pipe", 1);
+	pid = fork();
+	if (pid == -1)
+		error_exit("Failed to fork for here_doc", 1);
+	if (pid == 0)
+	{
+		close(pipe_fd[0]);
+		write(STDOUT_FILENO, "pipe heredoc> ", 14);
+		while (1)
+		{
+			line = get_next_line();
+			if (!line || ft_strcmp(line, limiter) == 0)
+			{
+				if (line)
+					free(line);
+				break;
+			}
+			write(pipe_fd[1], line, ft_strlen(line));
+			write(pipe_fd[1], "\n", 1);
+			free(line);
+			write(STDOUT_FILENO, "pipe heredoc> ", 14);
+		}
+		close(pipe_fd[1]);
+		exit(0);
+	}
+	close(pipe_fd[1]);
+	waitpid(pid, NULL, 0);
+	return (pipe_fd[0]);
+}
---
+++ bonus/utils.c
@@ -0,0 +1,87 @@
+
+#include "pipex_bonus.h"
+
+void	error_exit(char *message, int i)
+{
+	write(2, message, ft_strlen(message));
+	write(2, "\n", 1);
+	exit(i);
+}
+
+int	open_file(char *filename, int flags, int mode)
+{
+	int	fd;
+
+	fd = open(filename, flags, mode);
+	if (fd == -1)
+		error_exit("Error opening file", 1);
+	return (fd);
+}
+
+char	*get_env_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*find_command_path(char *cmd, char **envp, int i)
+{
+	char	**paths;
+	char	*chars[3];
+
+	if (access(cmd, F_OK) == 0)
+		return (ft_strdup(cmd));
+	chars[0] = get_env_path(envp);
+	if (!chars[0])
+		return (NULL);
+	paths = ft_split(chars[0], ':');
+	if (!paths)
+		return (NULL);
+	while (paths[i])
+	{
+		chars[2] = ft_strjoin(paths[i++], "/");
+		chars[1] = ft_strjoin(chars[2], cmd);
+		free(chars[2]);
+		if (access(chars[1], F_OK) == 0)
+		{
+			cleanup_array(paths);
+			return (chars[1]);
+		}
+		free(chars[1]);
+	}
+	cleanup_array(paths);
+	return (NULL);
+}
+
+void	execute_command(char *cmd_str, char **envp)
+{
+	char	**cmd_args;
+	char	*cmd_path;
+
+	cmd_args = ft_split(cmd_str, ' ');
+	if (!cmd_args || !cmd_args[0])
+	{
+		cleanup_array(cmd_args);
+		error_exit("Invalid command", 127);
+	}
+	cmd_path = find_command_path(cmd_args[0], envp, 0);
+	if (!cmd_path)
+	{
+		cleanup_array(cmd_args);
+		error_exit("Command not found", 127);
+	}
+	if (execve(cmd_path, cmd_args, envp) == -1)
+	{
+		free(cmd_path);
+		cleanup_array(cmd_args);
+		error_exit("Command execution failed", 1);
+	}
+}
